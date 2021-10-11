fd74fb6d923874d7779392dc0cdf8fef8973c0c9
mohon63 <mohonhossen63@gmail.com>
2021-10-11 07:29:32 +0600

    authentication

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCWo3LB_KM4DQ80LQVi45T79N0Cw2yy-n4",
+    authDomain: "simple-firebase-authenti-fbe8c.firebaseapp.com",
+    projectId: "simple-firebase-authenti-fbe8c",
+    storageBucket: "simple-firebase-authenti-fbe8c.appspot.com",
+    messagingSenderId: "965811729773",
+    appId: "1:965811729773:web:e5600f344d4715c244d664"
+};
+
+export default firebaseConfig;
